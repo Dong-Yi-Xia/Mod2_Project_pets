40abab905721645f2bef894d682f1a91c548c5dc
Dong-Yi-Xia <dong7777xia@gmail.com>
2020-09-05 13:51:22 -0400

    added login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+
+    helper_method :logged_in_status
+    before_action :authorization? 
+
+
+    def current_user
+        @current_user = User.find_by(id: session[:user_id])
+    end
+
+    def logged_in_status
+        !current_user.nil?
+    end
+
+    def authorization? 
+        redirect_to login_path unless logged_in_status
+        # if !logged_in_status
+        #     redirect_to login_path
+        # end
+    end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,10 @@ Rails.application.routes.draw do
   resources :pets
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "/login", to: "users#login", as: "login"
+  post "/login_form", to: "users#handle_login"
+  delete "/logout", to: "users#logout", as: "logout"
+
+
 end
