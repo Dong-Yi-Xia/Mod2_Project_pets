a1689a600f8fdcaf422c1d4525bba75842b5d951
emjose <emjose@gmail.com>
2020-09-06 13:37:50 -0400

    added readme draft

--- README.md
+++ README.md
@@ -1,4 +1,56 @@
-# README
+# "Pet" App
+
+The "Pet" App is inspired by our cohort's love of dogs and all rescue animals.
+
+The user is presented with a fun platform of pets and not-so-ordinary pets, with several options: 
+* RENT: The user can book a rental "play-date" with the cute pet of their choice.
+* ADOPT: The user can adopt a pet and give him or her their forever home.
+* REVIEW: The user can rate and write a review of pets they have booked for play-dates and/or adopted.
+
+The app implements Ruby on Rails MVC design structures and CRUD functionality.
+
+# Domain-Modeling
+
+2 independent models: User, Pet
+3 joinder models: Rent, Adoption, Review
+
+User ---< Rent >--- Pet
+User ---< Adoption >--- Pet
+User ---< Review >--- Pet
+
+***
+## Getting Started
+To run the "Pet" App, you will need to use the terminal on your computer.
+
+```
+    git clone https://github.com/Dong-Yi-Xia/Mod2_Project_pets
+```
+
+## Installation and Running the Application
+
+After cloning the repository onto your computer, please run the following commands, one at a time, in your terminal:
+```
+    bundle install
+    rake db:migrate
+    rake db:seed
+```
+To run the App, type the following into the terminal:
+```
+    ruby bin/run.rb 
+```
+
+## Contributors
+
+Dong Xia
+
+Emmanuel Jose
+
+## Resources
+
+https://unsplash.com/
+
+
+<!-- # README
  read me again
 This README would normally document whatever steps are necessary to get the
 application up and running.
@@ -23,5 +75,6 @@ Things you may want to cover:
 
 * ...
 
-Emmanuel Jose .....
+Emmanuel Jose ..... -->
+
 
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,42 +1,43 @@
 class ReviewsController < ApplicationController
 
-    # def index 
-    #     @reviews = Review.all
-    # end
+    def index 
+        @reviews = Review.all
+    end
      
-    # def show 
-    #     @review = Review.find(params[:id])
-    # end 
+    def show 
+        @review = Review.find(params[:id])
+    end 
   
-    # def new
-    #     @review = Review.new
-    # end 
+    def new
+        @review = Review.new
+    end 
   
-    # def create
-    #     @review = Review.create(review_params)
-    #     redirect_to review_path(@review)
-    # end 
+    def create
+        @review = Review.create(review_params)
+        redirect_to review_path(@review)
+    end 
   
-    # def edit
-    #     @review = Review.find(params[:id])
-    # end 
+    def edit
+        @review = Review.find(params[:id])
+    end 
   
-    # def update
-    #     @review = Review.find(params[:id])
-    #     @review.update(review_params)
-    #     redirect_to review_path(@review)
-    # end
+    def update
+        @review = Review.find(params[:id])
+        @review.update(review_params)
+        redirect_to review_path(@review)
+    end
   
-    # def destroy
-    #     @review = Review.find(params[:id])
-    #     @review.delete 
-    #     redirect_to reviews_path
-    # end 
+    def destroy
+        @review = Review.find(params[:id])
+        @review.delete 
+        redirect_to reviews_path
+    end 
   
-    # private
+    private
   
-    # def review_params
-    #     params.require(:review).permit(:rating, :content)
-    # end 
+    def review_params
+        params.require(:review).permit(:rating, :content)
+    end 
+
 
 end
