5f1d4b80c58924ae06a4fd7d4f19f201cf9092ac
emjose <emjose@gmail.com>
2020-09-07 13:29:57 -0400

    added optional reviews controller create method as comment

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -16,6 +16,16 @@ class ReviewsController < ApplicationController
         @review = @current_user.reviews.create(review_params)
         redirect_to user_path(@current_user)
     end 
+
+    # def create 
+    #     @review = Review.new(review_params)
+    #     if @review.save 
+    #         redirect_to "/pets/#{pet.id}"
+    #     else
+    #         flash.now[:notice] = "One or more fields is blank."
+    #         render :new
+    #     end
+    # end
   
     def edit
         byebug
