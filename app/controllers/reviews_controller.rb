06319d08f55eae0177e8be6d421927ac01bd73dc
Dong-Yi-Xia <dong7777xia@gmail.com>
2020-09-08 15:58:36 -0400

    review update

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -36,7 +36,7 @@ class ReviewsController < ApplicationController
     def destroy
         @review = Review.find(params[:id])
         @review.destroy
-        redirect_to reviews_path
+        redirect_to user_path(@current_user)
     end 
   
     private
