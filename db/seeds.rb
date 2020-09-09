5c1955c87009f3b7dd107502ebe722c2350f149d
emjose <emjose@gmail.com>
2020-09-09 14:23:24 -0400

    images added to pets view

--- db/seeds.rb
+++ db/seeds.rb
@@ -53,6 +53,7 @@ bio17 = "Ruby is an adorable, loyal girl who always has your back. She loves to 
 bio18 = "Deeply misunderstood and wrongly judged for his appearance, Li’l Britches would love nothing more than to cuddle in your hand and listen to you talk all day."
 bio19 = "Glimmer is a beautiful master of her domain and has big eyes for the world. Although she has a short attention span, she’s always happy to see you!"
 bio20 = "Peanut would love to have a forever home with a software engineer. Peanut loves to code and may or may not know all the secrets of the universe. Won’t you give him a home?"
+bio21 = "Fierce and loyal, Korra is a bad-ass and has an extraordinary ability to bend all elements around her, as long she gets plenty of love, treats, and toys!"
 
 # variable = "string"
 
@@ -61,26 +62,27 @@ bio20 = "Peanut would love to have a forever home with a software engineer. Pean
 dong = User.create(name:"Dong", password: "abc123" )
 manny = User.create(name:"Manny", password: "123abc")
 
-pet1 = Pet.create(name: "Sugar", species: species_d, gender: gender_f, bio: bio1, adoption_status: "yes")
-pet2 = Pet.create(name: "Charlie", species: species_d, gender: gender_m, bio: bio2, adoption_status: "yes")
-pet3 = Pet.create(name: "Tabitha", species: species_c, gender: gender_f, bio: bio3, adoption_status: "yes")
-pet4 = Pet.create(name: "Fritz", species: species_c, gender: gender_m, bio: bio4, adoption_status: "yes")
-pet5 = Pet.create(name: "Honey Bunny", species: species_r, gender: gender_f, bio: bio5, adoption_status: "yes")
-pet6 = Pet.create(name: "Pupperoni", species: species_d, gender: gender_m, bio: bio6, adoption_status: "yes")
-pet7 = Pet.create(name: "Lily", species: species_d, gender: gender_f, bio: bio7, adoption_status: "yes")
-pet8 = Pet.create(name: "Bubs", species: species_c, gender: gender_m, bio: bio8, adoption_status: "yes")
-pet9 = Pet.create(name: "Willow", species: species_r, gender: gender_f, bio: bio9, adoption_status: "yes")
-pet10 = Pet.create(name: "Wolf", species: species_d, gender: gender_m, bio: bio10, adoption_status: "no")
-pet11 = Pet.create(name: "Missy", species: species_d, gender: gender_f, bio: bio11, adoption_status: "no")
-pet12 = Pet.create(name: "Doc", species: species_c, gender: gender_m, bio: bio12, adoption_status: "no")
-pet13 = Pet.create(name: "Mimosa", species: species_c, gender: gender_f, bio: bio13, adoption_status: "no")
-pet14 = Pet.create(name: "Boom Boom", species: species_r, gender: gender_m, bio: bio14, adoption_status: "no")
-pet15 = Pet.create(name: "Xena", species: species_d, gender: gender_f, bio: bio15, adoption_status: "no")
-pet16 = Pet.create(name: "Ollie", species: species_d, gender: gender_m, bio: bio16, adoption_status: "no")
-pet17 = Pet.create(name: "Ruby", species: species_d, gender: gender_f, bio: bio17, adoption_status: "no")
-pet18 = Pet.create(name: "Li’l Britches", species: species_s, gender: gender_m, bio: bio18, adoption_status: "no")
-pet19 = Pet.create(name: "Glimmer", species: species_f, gender: gender_f, bio: bio19, adoption_status: "no")
-pet20 = Pet.create(name: "Peanut", species: species_p, gender: gender_m, bio: bio20, adoption_status: "no")
+pet1 = Pet.create(name: "Sugar", species: species_d, gender: gender_f, bio: bio1, adoption_status: "yes", image_url: "Pet_01_Sugar.jpg")
+pet2 = Pet.create(name: "Charlie", species: species_d, gender: gender_m, bio: bio2, adoption_status: "yes", image_url: "Pet_02_Charlie.jpg")
+pet3 = Pet.create(name: "Tabitha", species: species_c, gender: gender_f, bio: bio3, adoption_status: "yes", image_url: "Pet_03_Tabitha.jpg")
+pet4 = Pet.create(name: "Fritz", species: species_c, gender: gender_m, bio: bio4, adoption_status: "yes", image_url: "Pet_04_Fritz.jpg")
+pet5 = Pet.create(name: "Honey Bunny", species: species_r, gender: gender_f, bio: bio5, adoption_status: "yes", image_url: "Pet_05_Honey_Bunny.jpg")
+pet6 = Pet.create(name: "Pupperoni", species: species_d, gender: gender_m, bio: bio6, adoption_status: "yes", image_url: "Pet_06_Pupperoni.jpg")
+pet7 = Pet.create(name: "Lily", species: species_d, gender: gender_f, bio: bio7, adoption_status: "yes", image_url: "Pet_07_Lily.jpg")
+pet8 = Pet.create(name: "Bubs", species: species_c, gender: gender_m, bio: bio8, adoption_status: "yes", image_url: "Pet_08_Bubs.jpg")
+pet9 = Pet.create(name: "Willow", species: species_r, gender: gender_f, bio: bio9, adoption_status: "yes", image_url: "Pet_09_Willow.jpg")
+pet10 = Pet.create(name: "Wolf", species: species_d, gender: gender_m, bio: bio10, adoption_status: "no", image_url: "Pet_10_Wolf.jpg")
+pet11 = Pet.create(name: "Missy", species: species_d, gender: gender_f, bio: bio11, adoption_status: "no", image_url: "Pet_11_Missy.jpg")
+pet12 = Pet.create(name: "Doc", species: species_c, gender: gender_m, bio: bio12, adoption_status: "no", image_url: "Pet_12_Doc.jpg")
+pet13 = Pet.create(name: "Mimosa", species: species_c, gender: gender_f, bio: bio13, adoption_status: "no", image_url: "Pet_13_Mimosa.jpg")
+pet14 = Pet.create(name: "Boom Boom", species: species_r, gender: gender_m, bio: bio14, adoption_status: "no", image_url: "Pet_14_Boom_Boom.jpg")
+pet15 = Pet.create(name: "Xena", species: species_d, gender: gender_f, bio: bio15, adoption_status: "no", image_url: "Pet_15_Xena.jpg")
+pet16 = Pet.create(name: "Ollie", species: species_d, gender: gender_m, bio: bio16, adoption_status: "no", image_url: "Pet_16_Ollie.jpg")
+pet17 = Pet.create(name: "Ruby", species: species_d, gender: gender_f, bio: bio17, adoption_status: "no", image_url: "Pet_17_Ruby.jpg")
+pet18 = Pet.create(name: "Li’l Britches", species: species_s, gender: gender_m, bio: bio18, adoption_status: "no", image_url: "Pet_18_Lil_Britches.jpg")
+pet19 = Pet.create(name: "Glimmer", species: species_f, gender: gender_f, bio: bio19, adoption_status: "no", image_url: "Pet_19_Glimmer.jpg")
+pet20 = Pet.create(name: "Peanut", species: species_p, gender: gender_m, bio: bio20, adoption_status: "yes", image_url: "Pet_20_Peanut.jpg")
+pet21 = Pet.create(name: "Korra", species: species_d, gender: gender_f, bio: bio21, adoption_status: "yes", image_url: "Pet_21_Korra.jpg")
 
 # pet seed data in long form
 
